69e9d971a20dea938930820d7584409fc0fc7b2a
damienroche <damienroche@free.fr>
2025-04-03 22:01:30 +0200

    Add test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <script lang="ts" setup>
 import Vehicles from '~/components/Vehicles.vue'
 const store = useStore()
-store.drivers.queryMany()
+
+// Console log => "drivers"
+// store.drivers.queryMany(),
+// store.vehicleConfigurations.queryMany(),
+// store.vehicles.queryMany()
+
+// Console log => "drivers" / "vehicleConfigurations"
+// await Promise.all([
+//  store.drivers.queryMany(),
+//  store.vehicleConfigurations.queryMany(),
+//  store.vehicles.queryMany()
+//])
+
+// Console log => "drivers"
+Promise.all([
+  store.drivers.queryMany(),
+  store.vehicleConfigurations.queryMany(),
+  store.vehicles.queryMany()
+])
+
 </script>
 
 <template>
